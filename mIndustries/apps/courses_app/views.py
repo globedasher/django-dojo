16494587a988a487c7fd6137cbe2543d27756991
globedasher <globe.dasher@gmail.com>
2016-11-21 14:41:03 -0800

    The multiple apps assignment is as good as I'm getting it right now.

--- mIndustries/apps/courses_app/views.py
+++ mIndustries/apps/courses_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib import messages
 
+from django.db.models import Count
 from .models import Course
 from ..authentication.models import User
 
@@ -18,14 +19,11 @@ def index(request):
 
 
 def show(request):
+    # I have to get the following two queries to fill the selection lists.
     courses = Course.objects.all()
     users = User.objects.all()
-    # Hmm... I thought I had a grasp of the ORM ideas, but this next filter is
-    # throwing me for a loop!
-    course_filter = Course.objects.filter(student__courses=2).count()
-    #for course in course_filter:
-    #    print(type(course.student))
-    #    print(course.course_name)
+    # This next query is to get a count of the students in each course.
+    course_filter = Course.objects.annotate(number=Count('student'))
     context = { 'courses': courses, 'users': users, 'course_filter': course_filter }
     return render(request, 'courses_app/user_course.html', context)
 
@@ -54,13 +52,18 @@ def assign(request):
         # table.
         user_id = request.POST['users']
         course_id = request.POST['courses']
-        tuple_return = Course.objects.add_user_to_course(user_id=user_id
-                , course_id=course_id)
-        messages.success(request, "You have added a user to a course.")
-        return redirect(reverse('courses:show'))
+        try:
+            tuple_return = Course.objects.add_user_to_course(user_id=user_id
+                    , course_id=course_id)
+            messages.success(request, "You have added a user to a course.")
+            print(tuple_return[1].__dict__)
+            return redirect(reverse('courses:show'))
+        except:
+            messages.error(request, "Registation did not work.")
+            return redirect(reverse('courses:show'))
     else:
         messages.error(request, 'Method was not POST.')
-        return redirect(reverse('course:show'))
+        return redirect(reverse('courses:show'))
 
 
 def delete(request, course_id):
